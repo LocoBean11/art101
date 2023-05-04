bf087b8cfe196fbf11517ca7a904e004dd37c789
LocoBean11 <aatrodri@ucsc.edu>
2023-05-03 17:12:00 -0700

    all

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -4,16 +4,16 @@
 
 //sort userName - function that takes user input and sorts the letters of their name
 function sortUserName() {
-var username = window.prompt("Hello. Please tell me your name so I can fix it.");
+var userName = window.prompt("Hello. Please tell me your name so I can fix it.");
 console.log("userName =", userName);
 //This splits string to an array
-var nameArray = username.split('');
+var nameArray = userName.split('');
 console.log("nameArray =", nameArray);
 //This sorts the array
-var nameArraySort = nameArraySort.sort();
-console.log("nameArraySort =", nameArraySort);
+var nameArray = nameArray.sort();
+console.log("nameArraySort =", nameArray);
 //This joins the array back to a string
-var nameSorted = nameArraySort.join('');
+var nameSorted = nameArray.join('');
 console.log("nameSorted =", nameSorted);
 
 return nameSorted;
