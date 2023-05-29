b3710fceb0bc12d6bf0c514447d7f22c694bc002
LocoBean11 <aatrodri@ucsc.edu>
2023-05-29 15:12:20 -0700

    all

---
+++ lab14/js/lab.js
@@ -0,0 +1,30 @@
+    // index.js - Loops
+    // Author: Aaron Rodriguez, Ethan Christian
+    // Date: May 29th, 2023
+
+    //Find the element
+    output = $('#output');
+
+    //Fizzbuzz function that loops through 200
+    for (i = 0; i<=200; i++){
+    var str = '';
+
+    if(i%3==0){
+        str+="Fizz";
+    }
+        else if(i%5==0){
+            str+='Buzz';
+
+        }
+        else if(i%7==0){
+            str+='Boom';
+
+        }
+
+        if(i%3==0 | i%5==0 | i%7==0){
+            output.append(str+'! <br>');
+        }
+        else{
+            output.append(i+'<br>');
+        }
+    }
