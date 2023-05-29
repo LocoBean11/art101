012337a2d5cb29accf467fcd7d038d12b3d0f715
LocoBean11 <aatrodri@ucsc.edu>
2023-05-29 16:20:42 -0700

    all

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -3,28 +3,13 @@
     // Date: May 29th, 2023
 
     //Find the element
-    output = $('#output');
+    outputEl=$('#output');
 
-    //Fizzbuzz function that loops through 200
-    for (i = 0; i<=200; i++){
-    var str = '';
-
-    if(i%3==0){
-        str+="Fizz";
+    //Simple code to test debugger
+    function calculateSum(a, b){
+        const sum = a + b;
+        debugger;
+        return sum;
     }
-        else if(i%5==0){
-            str+='Buzz';
 
-        }
-        else if(i%7==0){
-            str+='Boom';
-
-        }
-
-        if(i%3==0 | i%5==0 | i%7==0){
-            output.append(str+'! <br>');
-        }
-        else{
-            output.append(i+'<br>');
-        }
-    }
+    console.log(calculateSum(3, 25));
